ee0f8e7dd6524ee7218466e925b6118ecb18797b
Niño Feliciano <ninofeliciano9@gmail.com>
2024-04-22 20:58:54 +0800

    platform-management page

--- resources/views/admin/platform_content/platform_content.blade.php
+++ resources/views/admin/platform_content/platform_content.blade.php
@@ -2,13 +2,63 @@
 @extends('admin.layout.layout')
 
 @section('content')
-<div>
-    @foreach ($pcontents as $content)
-    <form id="form-platform-content-{{$content->container}}">
-        <h4>{{$content->container}}</h4>
-        <textarea rows="" cols="">{{$content->content}}</textarea>
-        <button type="submit">Save</button>
-    </form>
-    @endforeach
+<div data-elementor-type="wp-page" data-elementor-id="1856" class="elementor elementor-1856"
+    data-elementor-post-type="page">
+    <div class="elementor-element elementor-element-3069eba e-flex e-con-boxed e-con e-parent"
+        data-id="3069eba" data-element_type="container"
+        data-settings="{&quot;background_background&quot;:&quot;classic&quot;,&quot;container_type&quot;:&quot;flex&quot;,&quot;content_width&quot;:&quot;boxed&quot;}"
+        data-core-v316-plus="true">
+        <div class="e-con-inner">
+            <div class="elementor-element elementor-element-67ba668 e-con-full e-flex e-con e-child"
+                data-id="67ba668" data-element_type="container"
+                data-settings="{&quot;content_width&quot;:&quot;full&quot;,&quot;container_type&quot;:&quot;flex&quot;}">
+                <div class="elementor-element elementor-element-41763c3 login-container e-flex e-con-boxed e-con e-child"
+                    data-id="41763c3" data-element_type="container">
+                    <div class="e-con-inner">
+                        <div class="elementor-element elementor-element-a63ff4e elementor-widget elementor-widget-heading"
+                            data-id="a63ff4e" data-element_type="widget" data-widget_type="heading.default">
+                            <div class="elementor-widget-container">
+                                <h4 class="elementor-heading-title elementor-size-default">SELECT SECTION
+                                </h4>
+                            </div>
+                        </div>
+                        <div class="elementor-element elementor-element-d3b7a2e elementor-widget elementor-widget-html"
+                            data-id="d3b7a2e" data-element_type="widget" data-widget_type="html.default">
+                            <div class="elementor-widget-container">
+                                <form> <select style="border-radius: 10px;">
+                                        <option value="select">Select</option>
+                                        <option value="Who we are">Who we are</option>
+                                        <option value="Who we are">Become a merchant</option>
+                                    </select> </form>
+                            </div>
+                        </div>
+                        <div class="elementor-element elementor-element-719c1ba elementor-widget elementor-widget-html"
+                            data-id="719c1ba" data-element_type="widget" data-widget_type="html.default">
+                            <div class="elementor-widget-container">
+                                <div> <textarea id="tinymce"></textarea> </div>
+                                <script>
+                                    $('textarea#tinymce').tinymce({
+                                        height: 300,
+                                        menubar: 'edit format'
+                                        /* other settings... */
+                                    });
+                                </script>
+                            </div>
+                        </div>
+                        <div class="elementor-element elementor-element-4d9c8a1 elementor-widget__width-inherit elementor-align-justify elementor-widget elementor-widget-button"
+                            data-id="4d9c8a1" data-element_type="widget" data-widget_type="button.default">
+                            <div class="elementor-widget-container">
+                                <div class="elementor-button-wrapper"> <a
+                                        class="elementor-button elementor-button-link elementor-size-sm"
+                                        href="#"> <span class="elementor-button-content-wrapper"> <span
+                                                class="elementor-button-text">SAVE</span> </span> </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 @endsection
