40a8466323544907fe306e4afbde35502b6bc23b
IK Mendoza <iken.mndz@gmail.com>
2024-08-06 20:36:17 +0800

    fx

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Payment extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['payment_id', 'payer_email', 'amount', 'currency', 'payment_status'];
 }
