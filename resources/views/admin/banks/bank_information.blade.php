36bbaff764b40e992b7fa14a128894e8459f1cb7
Niño Feliciano <ninofeliciano9@gmail.com>
2025-02-10 09:48:02 +0800

    update backend ui

--- resources/views/admin/banks/bank_information.blade.php
+++ resources/views/admin/banks/bank_information.blade.php
@@ -2,9 +2,44 @@
 @section('content')
 <div class="main-panel">
     <div class="content-wrapper">
+        <style>
+            .form-control[name="bank_name"]{
+                color: #495057;
+                outline: 1px solid #495057;
+            }
+            button[type="save"]{
+                background: #5f7a61 !important;
+                color: #FFF !important;
+                border-radius: 10px;
+                width: 100%;
+                max-width: calc(50% - 20px);
+            }
+            h3.font-weight-bold{
+                margin-bottom: 50px;
+            }
+
+            @media (max-width: 767px){
+                .row{
+                    position: relative;
+                }
+                h3.font-weight-bold{
+                    margin-bottom: 20px;
+                }
+                button[type="save"]{
+                    width: 100%;
+                }
+            }
+        </style>
         <form action="{{ url('admin/bank/edit') }}" method="post">
         @csrf
             <div class="row">
+                <button class="custom_btn_for_navbar_mobile dashboard_nav_btn navbar-toggler navbar-toggler-right d-lg-none align-self-center" type="button" data-toggle="offcanvas">
+                    <span class="icon-menu"></span>
+                </button>
+                <div class="col-12 col-xl-8 mb-4 mb-xl-0">
+                    <h3 class="font-weight-bold">Vendor Bank Details</h3>
+                </div>
+
                 <div class="col-md-6 grid-margin stretch-card">
                     <div class="card">
                         <div class="card-body">
