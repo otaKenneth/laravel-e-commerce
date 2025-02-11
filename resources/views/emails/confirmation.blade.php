0db6b25d7be6dcc9f7de6f01cee6b7cfeb4b815d
IK Mendoza <iken.mndz@gmail.com>
2025-02-11 22:14:26 +0800

    email fixes

--- resources/views/emails/confirmation.blade.php
+++ resources/views/emails/confirmation.blade.php
@@ -82,7 +82,7 @@
         <!--EMAIL SUBJECT: Confirm Your Kapiton Account -->
         <p style="margin: 0 0 20px 0;">Dear {{ $name }}<?php /* {{-- $name --}} */ ?>,</p>
         <p style="margin: 0 0 20px 0;">Thank you for signing up with Kapiton! To complete your registration, please confirm your account by clicking the button below:</p>
-        <a href="# <?php /* {{ url('/user/confirm/' . $code) }} */ ?>" style="font-weight: 500; text-transform: uppercase; border-radius: 100px; border: none; text-decoration: none !important; color: white !important; background: #1f1f22; padding: 8px; margin-top: 20px; display: block; width: fit-content; margin-bottom: 30px;">📣Confirm Account</a>
+        <a href="{{ url('/user/confirm/' . $code) }}" style="font-weight: 500; text-transform: uppercase; border-radius: 100px; border: none; text-decoration: none !important; color: white !important; background: #1f1f22; padding: 8px; margin-top: 20px; display: block; width: fit-content; margin-bottom: 30px;">📣Confirm Account</a>
         <p style="margin: 0 0 20px 0;">Once confirmed, you'll be able to access all the features of your new account.</p>
         <p style="margin: 0 0 20px 0;">If you need assistance or have any questions, feel free to reach out to us.</p>
         
--- resources/views/emails/vendor_confirmation.blade.php
+++ resources/views/emails/vendor_confirmation.blade.php
@@ -72,7 +72,7 @@
 
     <div style="box-sizing: border-box; padding: 20px; background: #f0f5f0; border-radius: 10px; max-width: 630px; width: 90%; margin-left: auto; margin-right: auto; font-family: 'Lexend', Sans-serif;">
         <!--EMAIL SUBJECT: Confirm Your Vendor Account -->
-        <p style="margin: 0 0 20px 0;">Dear {{ $business_name }},</p>
+        <p style="margin: 0 0 20px 0;">Dear {{ $name }},</p>
         <p style="margin: 0 0 20px 0;">Welcome to Kapiton! To activate your vendor account and get started, please click the link below:</p>
         <a style="font-weight: 500; text-transform: uppercase; border-radius: 100px 100px 100px 100px; border: none; text-decoration: none !important; color: white !important; background: #1f1f22; padding: 8px; margin-top: 20px; display: block; width: fit-content; margin-bottom: 30px;" href="# <?php /* {{ url('vendor/confirm/' . $code) }} */ ?>">📣Confirm Your Account</a>
         <p style="margin: 0 0 20px 0;">Once your account is confirmed, you'll gain access to all vendor tools and features to support your business.</p>
