05750cd8160153087bb09f3e974f06476440aae3
IK Mendoza <iken.mndz@gmail.com>
2025-02-12 11:03:55 +0800

    Admin - order detail prod image fix

--- resources/views/admin/orders/order_details.blade.php
+++ resources/views/admin/orders/order_details.blade.php
@@ -499,7 +499,7 @@
                                                 @endphp
 
                                                 <a target="_blank" href="{{ url('product/' . $product['product_id']) }}">
-                                                    <img src="{{ asset('front/images/product_images/small/' . $getProductImage) }}">
+                                                    <img src="{{ $getImage('front/images/product_images/small/' . $getProductImage) }}">
                                                 </a>
                                             </td>
                                             <td>{{ $product['product_code'] }}</td>
