1a94a718419884ab876d84e01afb2a9870d8dbb8
IK Mendoza <iken.mndz@gmail.com>
2025-02-19 19:40:08 +0800

    9 to 10

--- app/Http/Controllers/Front/VendorController.php
+++ app/Http/Controllers/Front/VendorController.php
@@ -27,7 +27,7 @@ class VendorController extends Controller
         $vendors = Vendor::where('status', 1)
             ->with('vendorbusinessdetails')
             ->withSum('vendorProductOrders', 'product_qty')
-            ->paginate(9);
+            ->paginate(10);
         // dd($vendors);
         return view('front.pages.merchants')->with(compact('vendors'));
     }
