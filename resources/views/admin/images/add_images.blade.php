7a4c6060915185506205e6e4ca791c6b6ddd1531
IK Mendoza <iken.mndz@gmail.com>
2025-02-25 20:47:29 +0800

    getImage in admin Images

--- resources/views/admin/images/add_images.blade.php
+++ resources/views/admin/images/add_images.blade.php
@@ -99,21 +99,12 @@
                                 </div>
                                 <div class="form-group">
                                     {{-- Show the product image, if any (if exits) --}}
-                                    @if (!empty($product['product_image']))
-                                        <img style="width: 120px;
-                                                    height: auto;
-                                                    aspect-ratio: 1 / 1;
-                                                    object-fit: cover;
-                                                    border-radius: 10px;" 
-                                                    src="{{ url('front/images/product_images/small/' . $product['product_image']) }}"> {{--  the 'small' image --}}
-                                    @else
-                                        <img style="width: 120px;
-                                                    height: auto;
-                                                    aspect-ratio: 1 / 1;
-                                                    object-fit: cover;
-                                                    border-radius: 10px;" 
-                                                    src="{{ url('front/images/product_images/small/no-image.png') }}"> {{--  the 'small' image --}}
-                                    @endif
+                                    <img style="width: 120px;
+                                        height: auto;
+                                        aspect-ratio: 1 / 1;
+                                        object-fit: cover;
+                                        border-radius: 10px;" 
+                                        src="{{ $getImage('front/images/product_images/small/', $product['product_image']) }}"> {{--  the 'small' image --}}
                                 </div>
 
                 
@@ -146,7 +137,7 @@
                                     @foreach ($product['images'] as $image) {{-- using the relationship 'images' --}}<tr>
                                             <td>{{ $image['id'] }}</td>
                                             <td>
-                                                <img src="{{ url('front/images/product_images/small/' . $image['image']) }}"> {{-- Small --}}
+                                                <img src="{{ $getImage('front/images/product_images/small/', $image['image']) }}"> {{-- Small --}}
                                                 {{-- Medium --}}
                                                 {{-- Large --}}
                                             </td>
