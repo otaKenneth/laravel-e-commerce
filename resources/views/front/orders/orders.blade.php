5402527c5ac0fe57f5d67a3c991d66ecaeb26363
IK Mendoza <iken.mndz@gmail.com>
2025-03-13 20:37:52 +0800

    fix Orders product image

--- resources/views/front/orders/orders.blade.php
+++ resources/views/front/orders/orders.blade.php
@@ -77,8 +77,6 @@
                                 data-widget_type="image.default"
                             >
                                 <div class="elementor-widget-container">
-                                @if (!empty($product->product->product_image) && file_exists($product_image_path))
-                                {{-- if the product image exists in BOTH database table AND filesystem (on server) --}}
                                 <img
                                     loading="lazy"
                                     decoding="async"
@@ -90,20 +88,6 @@
                                     srcset="{{ $product_image_path }} 846w, {{ $product_image_path }} 248w, {{ $product_image_path }} 768w, {{ $product_image_path }} 879w"
                                     sizes="(max-width: 800px) 100vw, 800px"
                                 >
-                                @else
-                                {{-- show the dummy image --}}
-                                <img
-                                    loading="lazy"
-                                    decoding="async"
-                                    width="800"
-                                    height="968"
-                                    src="{{ $getImage('front/images/product/', 'no-available-image.jpg') }}"
-                                    class="attachment-large size-large wp-image-422"
-                                    alt=""
-                                    srcset="{{ $getImage('front/images/product/', 'no-available-image.jpg') }} 846w, {{ $getImage('front/images/product/', 'no-available-image.jpg') }} 248w, {{ $getImage('front/images/product/', 'no-available-image.jpg') }} 768w, {{ $getImage('front/images/product/', 'no-available-image.jpg') }} 879w"
-                                    sizes="(max-width: 800px) 100vw, 800px"
-                                >
-                                @endif
                                 </div>
                             </div>
                         </div>
