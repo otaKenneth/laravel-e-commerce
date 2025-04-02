c34db13147d04dda85ef2451a4fdfb3a92e31140
Adrian Christopher Nebasa <103738863+Djin-Greninja@users.noreply.github.com>
2025-04-02 16:30:28 +0800

    Update "Copyright @ [YEAR]" to be udpated automatically every year.

--- resources/views/admin/layout/footer.blade.php
+++ resources/views/admin/layout/footer.blade.php
@@ -1,6 +1,13 @@
 <!-- partial:partials/_footer.html -->
 <footer class="footer">
     <div class="d-sm-flex justify-content-center justify-content-sm-between">
-        <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright © 2022. All rights reserved.</span>
+        <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">
+            Copyright © <span id="currentYear"></span>. All rights reserved.
+        </span>
     </div>
 </footer>
+
+<script>
+    // Update the year dynamically
+    document.getElementById('currentYear').textContent = new Date().getFullYear();
+</script>
