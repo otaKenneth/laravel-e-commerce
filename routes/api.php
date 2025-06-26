50d8320f45613eb87c80d1b960c0373505de6958
Joshua Dia <diajoshua05@example.com>
2025-06-26 20:26:44 +0800

    Backend: Home Page

--- routes/api.php
+++ routes/api.php
@@ -81,4 +81,7 @@ Route::namespace('App\Http\Controllers\API')->group(function() { // Route Groups
     Route::post('/paymongo/webhook', 'APIController@paymongoPaymentStatus');
 
     Route::post('/lalamove/webhook', 'APIController@lalamoveDeliveryStatus');
+
+
+    Route::get('index', 'IndexController@index'); // API Endpoint: GET http://
 });
